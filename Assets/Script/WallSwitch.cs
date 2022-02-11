cc586d8dfda175ae5e03b0cc8f48308c9d4eae6f
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-02-11 19:42:03 +0900

    スイッチで切り替わる壁を追加（ラムダ式の課題）

---
+++ Assets/Script/WallSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class WallSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject[] walls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //walls = GameObject.FindGameObjectsWithTag("SwitchWall");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        walls.ToList().ForEach(go => go.SetActive(!go.activeSelf));
+    }
+}
