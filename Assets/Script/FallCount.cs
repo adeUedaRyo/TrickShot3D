c9c5ee724f6356a334c1fc35eb184a91788387e4
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-02-14 19:22:37 +0900

    パーティクルのスクリプトを変更

--- Assets/Script/FallCount.cs
+++ Assets/Script/FallCount.cs
@@ -21,7 +21,8 @@ public class FallCount : MonoBehaviour
     {
         if(other.tag == "Target")
         {
-            successP.SetActive(true);
+            //successP.SetActive(true);
+            Instantiate(successP, new Vector3(this.transform.position.x, -1, this.transform.position.z), Quaternion.Euler(-90,0,0)) ;
             gM.Good();
         }
         else if(other.tag == "Player")
