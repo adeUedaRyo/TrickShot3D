20e84df037708f9e0bcaf15ac0431e712ce3fee9
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-03-06 19:02:36 +0900

    ステージ数に応じてボールの色を変更

--- Assets/Script/FallCount.cs
+++ Assets/Script/FallCount.cs
@@ -23,7 +23,7 @@ public class FallCount : MonoBehaviour
         {
             //ターゲットが落ちたら
             Instantiate(successP, new Vector3(this.transform.position.x, -1, this.transform.position.z), Quaternion.Euler(-90,0,0)) ;
-            Invoke("Good",2f);
+            Invoke("Good",1.2f);
         }
         else if(other.tag == "Player")
         {
